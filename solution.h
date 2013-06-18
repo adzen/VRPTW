8c7b6eaf1cdfcb8aaf6627bf960ec7618cdcf559
adzen <101adzen@gmail.com>
2013-06-18 21:06:33 +0800

    Define the interface.

--- solution.h
+++ solution.h
@@ -14,6 +14,7 @@ public:
 	void clear();
 	void random(int maxRoutes, const problem& input);
 	void random(const problem& input);
+	void solomon(const problem& input, bool farthest, double mu = 1, double lambda = 2, double alpha1 = 0);
 	void fitness(const problem& input);
 	static int cmp(const solution &solA, const solution &solB, const problem &input);
 	static bool fdominate(const solution &solA, const solution &solB);
